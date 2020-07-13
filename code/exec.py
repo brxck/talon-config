a742b3805dd66b8f5d5f81e01e7dc3d0ca3f350c
Brock McElroy <28877984+brxck@users.noreply.github.com>
2020-07-12 19:13:21 -0700

    Start processes using subprocess module

--- code/exec.py
+++ code/exec.py
@@ -1,11 +1,19 @@
-import os
+import re
+import subprocess
+from pathlib import Path
 from talon import Module
 
 mod = Module()
+
+unquoted_whitespace_pattern = re.compile(r"\s+(?=(?:[^'\"]*['\"][^'\"]*['\"])*[^'\"]*$)")
 
 
 @mod.action_class
 class Actions:
     def system_command(cmd: str):
-        """execute a command on the system"""
-        os.system(cmd)
+        """Execute a command on the system."""
+        command = unquoted_whitespace_pattern.split(cmd)
+        subprocess.Popen(command,
+                         cwd=Path.home(),
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
